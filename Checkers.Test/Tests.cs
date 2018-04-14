31b4c789edb99d8d97abc9385ec32611e1af8bdf
SiveMg <sivemgud@gmail.com>
2018-04-14 18:56:30 +0200

    Start board with 12 black and 12 white pieces

--- Checkers.Test/Tests.cs
+++ Checkers.Test/Tests.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
+using System.Linq;
+using Checkers;
+ 
+
 
 namespace Checkers.Test
 {
-    class Tests
+    [TestFixture]
+    public class Tests
     {
+       
+        [Test]
+        public void ABoardHas12BlackAnd12WhitePieces()
+        {
+            Checkers.Board b = new Board();
+            int blackCount = b.Pieces(Color.Black).Count();
+            int whiteCount = b.Pieces(Color.White).Count();
+            Assert.That(blackCount == 12);
+            Assert.That(whiteCount == 14);
+
+        }
+        
     }
 }
--- Checkers/Piece.cs
+++ Checkers/Piece.cs
@@ -4,7 +4,34 @@ using System.Text;
 
 namespace Checkers
 {
-    class Piece
+    public class Piece : IPiece
     {
+        public Piece(Color c, int pos)
+        {
+            Status = Status.Active;
+            Position = pos;
+            Color = c;
+        }
+
+        public virtual Status Status {get; set;}
+
+        public virtual Color Color { get; private set; }
+
+        public virtual int Position { get; private set; }
+
+        public IEnumerable<int> CapturingMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Move(int destination)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<int> NormalMoves(IBoard board)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
